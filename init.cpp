758acb79d53bb711a646cb2ee156483f3bc3ce2b
LTH2285 <yy_liutinghan@sina.com>
2023-08-26 15:13:34 +0800

    20230826 15:13

--- init.cpp
+++ init.cpp
@@ -27,32 +27,37 @@ bool Init::createTable()
 
     bool flag = true;
 
-    flag = flag && query.exec("CREATE TABLE IF NOT EXISTS User_information_table (\
+    flag = flag && query.exec("CREATE TABLE IF NOT EXISTS user_information_table (\
                         userID INTEGER PRIMARY KEY,\
                         username TEXT NOT NULL,\
                         password TEXT NOT NULL,\
                         userIP TEXT NOT NULL,\
                         userPORT INTEGER NOT NULL,\
                         photo BLOB DEFAULT 'default')");
+    qDebug() << flag;
 
-    flag = flag && query.exec("CREATE TABLE IF NOT EXISTS \"Friend_relationship_table\" (\
+    flag = flag && query.exec("CREATE TABLE IF NOT EXISTS friend_relationship_table (\
                         userIP TEXT,\
-                        Friend_user_ID TEXT");
+                        Friend_user_ID TEXT)");
+    qDebug() << flag;
 
     flag = flag && query.exec("CREATE TABLE IF NOT EXISTS group_info_table (\
                         groupID INTEGER PRIMARY KEY,\
-                        groupname TEXT;\
+                        groupname TEXT,\
                         managerID INTEGER)");
+    qDebug() << flag;
 
     flag = flag && query.exec("CREATE TABLE IF NOT EXISTS group_member_table (\
                         groupID INTEGER PRIMARY KEY,\
                         userID INTEGER)");
+    qDebug() << flag;
 
     flag = flag && query.exec("CREATE TABLE IF NOT EXISTS message_table (\
                         sendID INTEGER PRIMARY KEY,\
                         recvID INTEGER,\
                         message TEXT,\
                         send_time TEXT)");
+    qDebug() << flag;
 
     flag = flag && query.exec("CREATE TABLE IF NOT EXISTS user_login_table (\
                         userID INTEGER PRIMARY KEY,\
--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //    MainWindow w;
-    //    w.show();
+//    MainWindow w;
+//    w.show();
     Init init;
 
     init.createDatabase();
 
-    init.createTable();
-
+    bool b = init.createTable();
+    qDebug() << b;
     return a.exec();
 
-    //    if (init.createDatabase()) {
-    //            if (init.createUserTable()) {
-    //                qDebug() << "Database and user table created successfully!";
-    //            } else {
-    //                qDebug() << "User table creation failed.";
-    //            }
-    //        } else {
-    //            qDebug() << "Database connection failed.";
-    //        }
+//    if (init.createDatabase()) {
+//            if (init.createUserTable()) {
+//                qDebug() << "Database and user table created successfully!";
+//            } else {
+//                qDebug() << "User table creation failed.";
+//            }
+//        } else {
+//            qDebug() << "Database connection failed.";
+//        }
+
 }
