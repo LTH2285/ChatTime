690c81d5097d61c5bcffc29828c2c2bba5e55023
LTH2285 <yy_liutinghan@sina.com>
2023-08-26 17:34:05 +0800

    20230826 17:34

--- utls.cpp
+++ utls.cpp
@@ -19,3 +19,28 @@ QString Utls::getColumnNames(QSqlQueryModel *model)
   return columnNames;
 }
 
+bool Utls::creati(QString tableName, QString values)
+{
+    QSqlQueryModel model;
+    model.setQuery("SELECT * FROM "+ tableName);
+    QString cols = getColumnNames(&model);
+    QString sql = "INSERT INTO " + tableName + " (" + cols + ") VALUES(" + values + ")";
+    QSqlQuery query;
+    return query.exec(sql);
+}
+
+bool Utls::updatei(QString tableName, QString values, QString conditions)
+{
+
+}
+
+bool Utls::deletei(QString tableName, QString conditions)
+{
+
+}
+
+QSqlQuery Utls::researchi(QString tableName, QString conditions)
+{
+
+}
+
--- utls.h
+++ utls.h
@@ -13,6 +13,7 @@ public:
     Utls();
     QString getColumnNames(QSqlQueryModel *model);
     bool creati(QString tableName,QString values);
+//    values用单引号引好各个值，用英文逗号隔开
     bool deletei(QString tableName,QString conditions);
     bool updatei(QString tableName,QString values,QString conditions);
     QSqlQuery researchi(QString tableName,QString conditions);
