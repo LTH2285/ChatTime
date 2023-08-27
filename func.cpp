cf7558abed51340c1854ef3ec9c742247a1caf8c
LTH2285 <yy_liutinghan@sina.com>
2023-08-27 22:06:22 +0800

    20230827 22:06

--- func.cpp
+++ func.cpp
@@ -7,7 +7,13 @@ Func::Func()
 {
 }
 
-// 查询用户IP是否存在
+// 检查给定的用户ID是否在user_information_table中存在
+//
+// 参数:
+// - userID:要检查存在性的用户ID
+//
+// 返回值:
+// - 如果用户ID存在,则返回true,否则返回false
 bool Func::checkUserIDExist(int userID)
 {
     Utls utls;
@@ -25,7 +31,14 @@ bool Func::checkUserIDExist(int userID)
     }
 }
 
-// 注册新账户
+// 用给定的用户名和密码注册新账户
+//
+// 参数:
+// - username: 新账户的用户名
+// - password: 新账户的密码
+//
+// 返回值:
+// - 新注册账户的ID,如果有错误则返回-1
 int Func::account_register(const QString username, const QString password)
 {
     Utls utls;
@@ -51,7 +64,14 @@ int Func::account_register(const QString username, const QString password)
     }
 }
 
-// 登陆界面查询用户ID密码是否匹配
+// 检查给定用户ID的登录凭证是否匹配
+//
+// 参数:
+// - userID: 尝试登录的用户ID
+// - password: 用户输入的密码
+//
+// 返回值:
+// - 如果凭证匹配并登录成功,则返回true,否则返回false
 bool Func::loginFunction(int userID, const QString password)
 {
     Utls utls;
@@ -80,7 +100,13 @@ bool Func::loginFunction(int userID, const QString password)
     }
 }
 
-// 主界面查询当前用户个人信息(photo、userID、userName)
+// 获取给定用户的个人信息(头像、用户ID、用户名)
+//
+// 参数:
+// - userID: 要获取信息的用户ID
+//
+// 返回值:
+// - 包含用户头像、ID和用户名的SQL查询结果
 QSqlQuery Func::getUserInfo(int userID)
 {
     //    Init init;
@@ -103,7 +129,13 @@ QSqlQuery Func::getUserInfo(int userID)
     return query;
 }
 
-// 主界面查询当前在线好友
+// 获取给定用户的在线好友
+//
+// 参数:
+// - userID: 要获取在线好友的用户ID
+//
+// 返回值:
+// - 包含所有在线好友ID的SQL查询结果
 QSqlQuery Func::getOnlineFriends(int userID)
 {
     //    Init init;
@@ -126,7 +158,13 @@ QSqlQuery Func::getOnlineFriends(int userID)
     return QSqlQuery(); // Return an empty query if no online friends found;
 }
 
-// 主界面查询当前在线好友数
+// 获取给定用户的在线好友数量
+//
+// 参数:
+// - userID: 要获取在线好友数量的用户ID
+//
+// 返回值:
+// - 在线好友数量
 int Func::getOnlineFriendsCount(int userID)
 {
     //    Init init;
@@ -150,7 +188,13 @@ int Func::getOnlineFriendsCount(int userID)
     return 0; // Return 0 if no online friends found;
 }
 
-// 查询未通过的好友申请
+// 获取给定用户的未通过好友请求
+//
+// 参数:
+// - userID: 要获取未通过请求的用户ID
+//
+// 返回值:
+// - 包含未通过好友请求的SQL查询结果
 QSqlQuery Func::getPendingFriendRequests(int userID)
 {
     Utls utls;
@@ -159,7 +203,13 @@ QSqlQuery Func::getPendingFriendRequests(int userID)
     return query;
 }
 
-// 查询在群聊内的用户
+// 获取给定群聊的成员
+//
+// 参数:
+// - groupID: 要获取成员的群聊ID
+//
+// 返回值:
+// - 包含群聊成员的SQL查询结果
 QSqlQuery Func::getGroupChatMembers(int groupID)
 {
     Utls utls;
@@ -181,7 +231,14 @@ QSqlQuery Func::getGroupChatMembers(int groupID)
     return QSqlQuery(); // Return an empty query if no members found;
 }
 
-// 查询聊天历史记录（单聊）
+// 获取两用户间的聊天历史记录（单聊）
+//
+// 参数:
+// - userID1: 第一个用户的ID
+// - userID2: 第二个用户的ID
+//
+// 返回值:
+// - 包含两用户间聊天记录的SQL查询结果
 QSqlQuery Func::getChatHistory(int userID1, int userID2)
 {
     Utls utls;
